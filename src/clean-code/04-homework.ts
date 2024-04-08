33c86ddfcd531d3157c6a2c975fdda4e05db9879
Patricia Manzanero Jiménez <52755910+Ossyyrr@users.noreply.github.com>
2024-04-08 16:40:53 +0200

    homework solved

--- src/clean-code/04-homework.ts
+++ src/clean-code/04-homework.ts
@@ -1,52 +1,69 @@
 (() => {
+  // CONSOLA DEL NAVEGADOR
+
   // Resolver sin la triple condicional dentro del if
   // includes? arrays?
+
+  const fruitsByColor: Record<string, string[]> = {
+    red: ["cereza", "ciruela", "manzana", "fresa"],
+    yellow: ["piña", "banana"],
+    purple: ["moras", "uvas"],
+  };
+
   function isRedFruit(fruit: string): boolean {
-    if (fruit === "manzana" || fruit === "cereza" || fruit === "ciruela") {
-      return true;
-    } else {
-      return false;
-    }
+    return fruitsByColor["red"].includes(fruit);
   }
 
   // Simplificar esta función
   // switch? Object literal? validar posibles colores
+
   function getFruitsByColor(color: string): string[] {
-    if (color === "red") {
-      return ["manzana", "fresa"];
-    } else if (color === "yellow") {
-      return ["piña", "banana"];
-    } else if (color === "purple") {
-      return ["moras", "uvas"];
-    } else {
+    // if (color === "red") {
+    //   return ["manzana", "fresa"];
+    // } else if (color === "yellow") {
+    //   return ["piña", "banana"];
+    // } else if (color === "purple") {
+    //   return ["moras", "uvas"];
+    // } else {
+    //   throw Error("the color must be: red, yellow, purple");
+    // }
+
+    if (!Object.keys(fruitsByColor).includes(color))
       throw Error("the color must be: red, yellow, purple");
-    }
+
+    return fruitsByColor[color] || [];
   }
 
   // Simplificar esta función
-  let isFirstStepWorking = true;
-  let isSecondStepWorking = true;
-  let isThirdStepWorking = true;
-  let isFourthStepWorking = true;
+  let isFirstStep = true;
+  let isSecondStep = true;
+  let isThirdStep = true;
+  let isFourthStep = true;
 
   function workingSteps() {
-    if (isFirstStepWorking === true) {
-      if (isSecondStepWorking === true) {
-        if (isThirdStepWorking === true) {
-          if (isFourthStepWorking === true) {
-            return "Working properly!";
-          } else {
-            return "Fourth step broken.";
-          }
-        } else {
-          return "Third step broken.";
-        }
-      } else {
-        return "Second step broken.";
-      }
-    } else {
-      return "First step broken.";
-    }
+    // if (isFirstStep === true) {
+    //   if (isSecondStep === true) {
+    //     if (isThirdStep === true) {
+    //       if (isFourthStep === true) {
+    //         return "Working properly!";
+    //       } else {
+    //         return "Fourth step broken.";
+    //       }
+    //     } else {
+    //       return "Third step broken.";
+    //     }
+    //   } else {
+    //     return "Second step broken.";
+    //   }
+    // } else {
+    //   return "First step broken.";
+    // }
+    if (!isFirstStep) return "First step broken.";
+    if (!isSecondStep) return "Second step broken.";
+    if (!isThirdStep) return "Third step broken.";
+    if (!isFourthStep) return "Fourth step broken.";
+
+    return "Working properly!";
   }
 
   // isRedFruit
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
-import './style.css'
+import "./clean-code/04-homework";
+import "./style.css";
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+const app = document.querySelector<HTMLDivElement>("#app")!;
 
 app.innerHTML = `
   <h1>CleanCode y SOLID</h1>
   <span>Revisar la consola de JavaScript</span>
-`
-
+`;
