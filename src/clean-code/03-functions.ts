32d29a505f4fcb92d8eeffc3438e8d84ee1ad182
Patricia Manzanero Jiménez <52755910+Ossyyrr@users.noreply.github.com>
2024-04-08 13:09:15 +0200

    example 3 solved

--- src/clean-code/03-functions.ts
+++ src/clean-code/03-functions.ts
@@ -1,40 +1,39 @@
 (() => {
   // función para obtener información de una película por Id
-  function getAllMovies(movieId: string) {
-    console.log({ movieId });
+  function getMovieById(id: string) {
+    console.log({ id });
   }
 
   // función para obtener información de los actores de una película - Actors o Cast // id = movieId getMovieCast
-  function getAllMovieActors(id: string) {
+  function getMovieCastById(id: string) {
     console.log({ id });
   }
 
   // funcion para obtener el bio del actor por el id
-  function getUsuario(ActorId: string) {
-    console.log({ ActorId });
+  function getActorBioById(id: string) {
+    console.log({ ActorId: id });
+  }
+
+  interface Movie {
+    title: string;
+    description: string;
+    rating: number;
+    cast: string[];
   }
 
   // Crear una película
-  function movie(
-    title: string,
-    description: string,
-    rating: number,
-    cast: string[]
-  ) {
+  function createMovie({ title, description, rating, cast }: Movie) {
     console.log({ title, description, rating, cast });
   }
 
-  // Crea un nuevo actor
-  function createActorIfActorNotExists(
-    fullName: string,
-    birthdate: Date
-  ): boolean {
-    // tarea asincrona para verificar nombre
-    // ..
-    // ..
+  function checkFullName(fullName: string): boolean {
     if (fullName === "fernando") return false;
-
     console.log("Crear actor");
     return true;
+  }
+
+  // Crea un nuevo actor
+  function createActor(fullName: string, birthdate: Date): boolean {
+    return checkFullName(fullName);
   }
 })();
