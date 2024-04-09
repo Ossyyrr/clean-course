fd7f01dac1532fda8bc883f6e616706d15fc5eca
Patricia Manzanero Jiménez <52755910+Ossyyrr@users.noreply.github.com>
2024-04-09 15:25:23 +0200

    wip

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 // import "./clean-code/04-homework";
 // import "./clean-code/05-dry";
-import "./clean-code/06-clases-c";
+// import "./clean-code/06-clases-c";
+import "./clean-code/07-tarea";
 import "./style.css";
 
 const app = document.querySelector<HTMLDivElement>("#app")!;
