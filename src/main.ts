080d6e15be2a5da30219265e0ca26bd217f58c02
Patricia Manzanero Jiménez <52755910+Ossyyrr@users.noreply.github.com>
2024-04-10 09:36:11 +0200

    Coupling

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 // import "./clean-code/04-homework";
 // import "./clean-code/05-dry";
 // import "./clean-code/06-clases-c";
-import "./clean-code/07-tarea";
+// import "./clean-code/07-tarea";
+import "./code-smells/01-singleton.js";
 import "./style.css";
 
 const app = document.querySelector<HTMLDivElement>("#app")!;
