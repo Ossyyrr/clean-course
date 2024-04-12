df15bbae7b01f8ad2e44806f7079701afbe029a7
Patricia Manzanero Jiménez <52755910+Ossyyrr@users.noreply.github.com>
2024-04-12 13:29:42 +0200

    pattern adapter, open-close

--- src/solid/02-open-close-c.ts
+++ src/solid/02-open-close-c.ts
@@ -1,8 +1,17 @@
-import axios from "axios";
+// import axios from "axios";
 
 export class HttpClient {
+  //! Usando axios
+  //   async get(url: string) {
+  //     const { data, status } = await axios.get(url);
+  //     console.log(`Status: ${status}`);
+  //     return { data, status };
+  //   }
+
   async get(url: string) {
-    const { data, status } = await axios.get(url);
+    const resp = await fetch(url);
+    const data = await resp.json();
+    const status = resp.status;
     console.log(`Status: ${status}`);
     return { data, status };
   }
